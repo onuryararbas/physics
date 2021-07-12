8d8536a34c957abb0e979ec68d54c8e73caa6b74
Onur Yararbas <onuryararbas@gmail.com>
2021-07-12 15:58:04 -0700

    first physics fdile

---
+++ cannonBallFire.js
@@ -0,0 +1,28 @@
+/*
+This code serves to show how far a cannonball can go (in absence of air resistance)
+
+
+*/
+function fireNoRes(v0, angle, maxHeight){
+    //use the y dimension of cannonball travel to solve for time
+    //plug in time to find the distance traveled
+
+
+    //Here, I break velocity into x and y dimensions
+    let vY = Math.sin(angle)*v0;
+    let vX = Math.cos(angle)*v0;
+
+    let accelY = -(vY*vY)/(2*maxHeight);
+
+
+    let time = -vY/accelY;
+
+    let dist = vX * time; 
+
+    console.log("With these parameters: Initial speed " + v0 + 
+    ", Angle of fire " + angle + " and max Height " + maxHeight + 
+    ", the distance travelled in the x direction is " + dist);
+
+
+    
+}
